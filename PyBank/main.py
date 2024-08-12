26a99ab5161ed3bf412c70306c63d0906bf518cd
Your Username <domoboriowo14@gmail.com>
2024-08-12 18:57:51 -0400

    completed PyBank analysis

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,25 +1,51 @@
 import csv
 
+incVal = 0
+decVal = 0
+total = 0
+months = 0
+pre_rev = 0
+total_ch = 0
 data = csv.reader(open('Resources/budget_data.csv'))
+myReport = open('Analysis/budget_report.txt', 'w')
 
 header = next(data)
-months = 0
-total = 0
 
 for row in data:
-    months = months + 1
+    months += 1
     rev = int(row[1])
     total = total + rev
-    
+
+    # Average change
+    ch = rev - pre_rev
+
+    if pre_rev == 0:
+        ch = 0
+
+    total_ch += ch
+
+    # Greastest increase
+    if ch > incVal:
+        incVal = ch
+        incMon = row[0]
+
+    # Greatest decrease
+    if ch< decVal:
+        decVal = ch
+        decMon = row[0]
+
+    # reset variables for next row
+    pre_rev = rev
 
 output = f'''
     Financial Analysis
 ----------------------------
 Total Months: {months}
 Total: ${total:,}
-Average Change: $-8311.11
-Greatest Increase in Profits: Aug-16 ($1862002)
-Greatest Decrease in Profits: Feb-14 ($-1825558)
+Average Change: ${total_ch/(months-1):,.2f}
+Greatest Increase in Profits: {incMon} (${incVal:,})
+Greatest Decrease in Profits: {decMon} (${decVal:,})
 '''
 
 print(output)
+myReport.write(output)
